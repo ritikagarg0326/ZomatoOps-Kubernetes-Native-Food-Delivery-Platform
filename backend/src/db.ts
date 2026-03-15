0d84e298fe85b0f98064fea77651399cbf30f9ca
Ritika Garg <ritikagarg60814@gmail.com>
2026-03-15 19:46:10 +0530

    Delete backend directory

--- backend/src/db.ts
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2/promise';
-
-export let db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Veronica123@',
-    database: 'blog',
-
-});
